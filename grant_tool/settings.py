9e702112a47508628d7a2bad253e09804e0ae9c4
Kenneth Rebello <kennethrebello253@gmail.com>
2025-01-04 11:41:07 +0530

    Latest updates
    Added voice module

--- grant_tool/settings.py
+++ grant_tool/settings.py
@@ -13,9 +13,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMP_DIR = os.path.join(BASE_DIR,"templates")
+
 
 
 # Quick-start development settings - unsuitable for production
--- grant_tool/urls.py
+++ grant_tool/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from grant_writer_app.views import grant_proposal_form  # Import your view here
+from grant_writer_app.views import grant_writer_ui 
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('grant_writer/', include('grant_writer_app.urls')),  # Routes to app urls
-    path('', grant_proposal_form, name='home'),  # Root path now routes to a view
+    path('grant_writer/', include('grant_writer_app.urls')),  
+    path('', grant_writer_ui, name='home'), 
 ]
